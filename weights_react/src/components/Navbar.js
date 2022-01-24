9e37c133de9e8e75c43e9b78e306f0b7d070986e
EGM <ezegonmac01@gmail.com>
2022-01-24 12:08:13 +0100

    Created Navbar component

---
+++ weights_react/src/components/Navbar.js
@@ -0,0 +1,17 @@
+
+const Navbar = () => {
+    return(
+        <nav class="navbar">
+            <div class="navbar__logo">
+                <img src="/Logo_v1.png" alt="weights"/>
+            </div>
+            <div class="navbar__nav">
+                <h2><a href="/">New Weight</a></h2>
+                <h2><a href="/">Table</a></h2>
+                <h2><a href="/">Graph</a></h2>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
